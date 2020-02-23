4e555bbe8871f05856a9f1ee1d068a0bd6c88832
Dariusz Dabrowski <dariuszdabrowski001@gmail.com>
2020-02-23 16:09:13 +0100

    Added Enumeration Difficulty. Closes #5

---
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.github.dariuszdabrowski.recipeproject.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/github/dariuszdabrowski/recipeproject/domain/Recipe.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/domain/Recipe.java
@@ -27,6 +27,8 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -114,5 +116,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
