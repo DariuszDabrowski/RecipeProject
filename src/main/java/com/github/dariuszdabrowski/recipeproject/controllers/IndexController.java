58ede44aa7b1cf70e9d6bbc9712d9f03df4549c4
Dariusz Dabrowski <dariuszdabrowski001@gmail.com>
2020-02-24 13:26:29 +0100

    Added Query Method Examples. Closes #9

--- src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/IndexController.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.github.dariuszdabrowski.recipeproject.controllers;
 
+import com.github.dariuszdabrowski.recipeproject.domain.Category;
+import com.github.dariuszdabrowski.recipeproject.domain.UnitOfMeasure;
+import com.github.dariuszdabrowski.recipeproject.repositories.CategoryRepository;
+import com.github.dariuszdabrowski.recipeproject.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository,
+                           UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
         System.out.println("Make some changes...11");
+        System.out.println("Category Id is " + categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure Id is " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/github/dariuszdabrowski/recipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.github.dariuszdabrowski.recipeproject.repositories;
 import com.github.dariuszdabrowski.recipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
