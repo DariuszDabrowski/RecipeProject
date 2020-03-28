d4d4a9a70f15328c15ca0797c78f330d7d417a74
Dariusz Dabrowski <dariuszdabrowski001@gmail.com>
2020-03-28 20:28:47 +0100

    refactor

--- src/main/java/com/github/dariuszdabrowski/recipeproject/domain/Notes.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/domain/Notes.java
@@ -1,10 +1,12 @@
 package com.github.dariuszdabrowski.recipeproject.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = "recipe")
 @Entity
 public class Notes {
 
