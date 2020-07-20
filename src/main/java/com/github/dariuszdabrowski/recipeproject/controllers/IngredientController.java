2419b37934683447d4a76abd935d0793816897b3
Dariusz Dabrowski <dariuszdabrowski001@gmail.com>
2020-07-20 12:29:04 +0200

    adding new Ingredient. Closes #24

--- src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/IngredientController.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/IngredientController.java
@@ -1,6 +1,8 @@
 package com.github.dariuszdabrowski.recipeproject.controllers;
 
 import com.github.dariuszdabrowski.recipeproject.commands.IngredientCommand;
+import com.github.dariuszdabrowski.recipeproject.commands.RecipeCommand;
+import com.github.dariuszdabrowski.recipeproject.commands.UnitOfMeasureCommand;
 import com.github.dariuszdabrowski.recipeproject.services.IngredientService;
 import com.github.dariuszdabrowski.recipeproject.services.RecipeService;
 import com.github.dariuszdabrowski.recipeproject.services.UnitOfMeasureService;
@@ -40,6 +42,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
