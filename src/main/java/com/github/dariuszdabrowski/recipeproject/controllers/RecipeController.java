e04dc5a10ec7bf38367f2b076a56914fe3009a51
Dariusz Dabrowski <dariuszdabrowski001@gmail.com>
2020-08-06 21:21:32 +0200

    Adding Exception Handler. Closes #31

--- src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/github/dariuszdabrowski/recipeproject/controllers/RecipeController.java
@@ -1,11 +1,15 @@
 package com.github.dariuszdabrowski.recipeproject.controllers;
 
 import com.github.dariuszdabrowski.recipeproject.commands.RecipeCommand;
+import com.github.dariuszdabrowski.recipeproject.exceptions.NotFoundException;
 import com.github.dariuszdabrowski.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
 @Slf4j
 @Controller
 public class RecipeController {
@@ -54,4 +58,18 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
+    }
+
 }
